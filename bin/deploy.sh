3291992173859366899f011eedd1ddc857002c8d
Sadik Ay <sadikay91@gmail.com>
2018-10-18 23:12:16 +0300

    deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,15 +1,20 @@
-#/bin/bash
+# build
+vuepress build
 
-set -e
+# navigate into the build output directory
+cd docs/.vuepress/dist
 
-yarn website:build
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
-cp -r ./.vuepress/dist/. ../master/
-
-cd ../master/
-
+git init
 git add -A
-git commit -m 'Deploy'
-git push origin master
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:SeaDude/SeaDude.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:sadikay/email-dashboard.github.io.git master:gh-pages
 
 cd -
---
+++ docs/service-worker.js
@@ -0,0 +1,112 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "a3e03f3ba5e07e05aae14a8452567da2"
+  },
+  {
+    "url": "assets/css/1.styles.fb0253dd.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "assets/css/2.styles.ac2986cc.css",
+    "revision": "67e4f12ed91a03bc12e1a51aac7323dd"
+  },
+  {
+    "url": "assets/css/styles.5af8b55a.css",
+    "revision": "cdbcfbd9a0654e6beec824fcc18d54a3"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/1.fb0253dd.js",
+    "revision": "fe3aebd308f97884e54bac25226fd3be"
+  },
+  {
+    "url": "assets/js/2.ac2986cc.js",
+    "revision": "60fc58741cfb9975a3854e966f000c85"
+  },
+  {
+    "url": "assets/js/3.d4529193.js",
+    "revision": "3303ac0d11885fb2ac03483788af277b"
+  },
+  {
+    "url": "assets/js/4.f8533975.js",
+    "revision": "8b1f2c6234ff54c6b90622f65bc25736"
+  },
+  {
+    "url": "assets/js/5.6fcf7342.js",
+    "revision": "8265f8df65d7f85847d1ca6b8f8212d7"
+  },
+  {
+    "url": "assets/js/6.4f97b047.js",
+    "revision": "295a6d7fb360c2a7df7cdb2c5f7b84d8"
+  },
+  {
+    "url": "assets/js/7.0a183a37.js",
+    "revision": "176a2e71704ef2bc9b2cc60496d27367"
+  },
+  {
+    "url": "assets/js/app.5af8b55a.js",
+    "revision": "51f540df4135e73b54bd094eeec65836"
+  },
+  {
+    "url": "guide/deployment.html",
+    "revision": "c7c16b12d149b7c61b513c01550f02dc"
+  },
+  {
+    "url": "guide/development.html",
+    "revision": "5c3940574bbbf34213071e80e7aadca9"
+  },
+  {
+    "url": "guide/index.html",
+    "revision": "2167645f56f247c0944a6cf741bbba85"
+  },
+  {
+    "url": "guide/README-zh_CN.html",
+    "revision": "6b305ad968d6fe25f4dc2fda421dc01a"
+  },
+  {
+    "url": "img/logo.png",
+    "revision": "67ac3d2f27e0652d22c58cfdaceacd96"
+  },
+  {
+    "url": "index.html",
+    "revision": "e7e79f99f581bb422ead91707ab8def4"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
