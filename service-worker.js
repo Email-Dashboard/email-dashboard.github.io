d62fefc02ef9383fa68b7f47b38453cdd16a341c
Sadik Ay <sadikay91@gmail.com>
2018-10-22 16:31:55 +0300

    Deploy

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "70941e28ddfdd7e3bc5fde76e180cd1e"
+    "revision": "d01df9c42dead9424aa7051a2efcdf66"
   },
   {
     "url": "assets/css/1.styles.fb0253dd.css",
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "revision": "67e4f12ed91a03bc12e1a51aac7323dd"
   },
   {
-    "url": "assets/css/styles.279b1220.css",
+    "url": "assets/css/styles.a2fb2995.css",
     "revision": "cdbcfbd9a0654e6beec824fcc18d54a3"
   },
   {
@@ -72,28 +72,28 @@ self.__precacheManifest = [
     "revision": "81755fd57c0b31f71626edcd53ef6d4b"
   },
   {
-    "url": "assets/js/app.279b1220.js",
-    "revision": "bdbd55ad6984b02922ab30a23f6a5fe8"
+    "url": "assets/js/app.a2fb2995.js",
+    "revision": "c479254cec0b26b6440ec437348add12"
   },
   {
     "url": "guide/deployment.html",
-    "revision": "18fe6a4aee42181ad6c67f66aad7d897"
+    "revision": "9f56bc4e50bc26129570cf2192dc30ca"
   },
   {
     "url": "guide/development.html",
-    "revision": "860126e24608576cc9b9a4c7aaa94ddf"
+    "revision": "567c45f3fa9008fdb8caa5fc0f8ae5b9"
   },
   {
     "url": "guide/index.html",
-    "revision": "73e9ea24eb2e603f123690f1909718e0"
+    "revision": "da1e996f99393482279731147e61e44e"
   },
   {
     "url": "guide/licence.html",
-    "revision": "a2bc2aa803bf4250802aeae1061e5ed7"
+    "revision": "fbc98e8899d51063396bdd533077315b"
   },
   {
     "url": "guide/README-zh_CN.html",
-    "revision": "c565e2029ca0cd38075766d257a06691"
+    "revision": "7c93db23fc51fd2aa1eb1cb7f7278f62"
   },
   {
     "url": "img/logo.png",
@@ -101,7 +101,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "edc5eab5ae4f2b656b09649eff8d75d1"
+    "revision": "f13017dbce1cf1c4cc63844b10556cc3"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
